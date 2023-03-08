69d286bfe06496a4be0bab73f63bc3c6f8cca15a
Fábio Sander Novaes Corrêa <fabio.correa@linedata.com.br>
2023-03-08 11:51:49 -0300

    Add AutoMapperConfig

---
+++ ConvenTech.Application/AutoMapper/ConvenTechAutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ConvenTech.Application.Dto;
+using ConvenTech.Domain.Account;
+
+namespace ConvenTech.Application.AutoMapper
+{
+    public class ConvenTechAutoMapperConfig: Profile
+    {
+        public ConvenTechAutoMapperConfig()
+        {
+            CreateMap<Usuario, UsuarioDto>();
+            CreateMap<UsuarioDto, Usuario>();
+        }
+    }
+}
